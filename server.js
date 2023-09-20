8056460b4191b744949464244cadb286cd91046a
mossouri <emmanuelkusiboamah@gmail.com>
2023-09-20 21:10:28 +0000

    setting server with routes and initiating

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("Hello server running...")
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+// Declaring routes for the app or endpoints
+app.get('/', (req, res)=>{
+    res.send('Hello is working...')
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}...`)
+});
