ce78d2ab4c17a834c16fe02a9b4a9e9637619e94
mossouri <emmanuelkusiboamah@gmail.com>
2023-09-21 18:30:03 +0000

    refactoring for clean code

---
+++ routes/productRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const Product = require('../models/productModels')
+const {getProducts, getProduct, createProduct, updateProduct, deleteProduct} = require('../controllers/productController')
+
+const router = express.Router();
+
+// query for all products in database
+router.get('/', getProducts);
+
+//get productby id
+router.get('/:id', getProduct);
+
+// creaate new product
+router.post('/', createProduct);
+
+// update a product
+router.put('/:id', updateProduct);
+
+// delete a product
+
+router.delete('/:id', deleteProduct);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,86 @@
-const express = require("express");
-const app = express();
+// require('dotenv').config();
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const productRoutes = require('./routes/productRoutes')
+// const app = express();
 
-const port = 3000;
+// // adding middleware
+// app.use(express.json());
+// // app.use('/api', productRoutes);
+// app.use('/api/products', productRoutes);
 
-// Declaring routes for the app or endpoints
-app.get('/', (req, res)=>{
-    res.send('Hello is working...')
+
+// const PORT = process.env.PORT || 3000 
+// // const PORT = 3000
+// // protect my mongodb driver and saving it at env file
+// const MONGO_URL = process.env.MONGO_URL
+
+// // Declaring routes for the app or endpoints
+// app.get("/", (req, res) => {
+//   res.send("Hello is working...");
+// });
+
+
+// mongoose.set("strictQuery", false);
+
+// // connecting mongoose database
+// mongoose
+//   .connect(MONGO_URL)
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(`Server running on port ${PORT}...`);
+//     });
+//     console.log("Connected to MongoDB database...");
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const proudctRoute = require('./routes/productRoutes');
+const errorMiddleware = require('./middleware/errorMiddleware')
+var cors = require('cors')
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+const MONGO_URL = process.env.MONGO_URL
+const FRONTEND = process.env.FRONTEND
+
+var corsOptions = {
+    origin: FRONTEND,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+//routes
+
+app.use('/api/products', proudctRoute);
+
+app.get('/', (req, res) => {
+    res.send('Hello NODE API')
 })
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}...`)
-});
+app.get('/blog', (req, res) => {
+    res.send('Hello Blog, My name is Emmanuel')
+})
+
+app.use(errorMiddleware);
+
+mongoose.set("strictQuery", false)
+mongoose.
+connect(MONGO_URL)
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(PORT, ()=> {
+        console.log(`Node API app is running on port ${PORT}`)
+    });
+}).catch((error) => {
+    console.log(error)
+})
